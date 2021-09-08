6a86b15c9916b314c2af66ffb0f8362683091f2d
Simeen Khan Ayyub <55340776+Simeen01@users.noreply.github.com>
2021-09-08 19:58:28 +0530

    Update TotalBill.java

--- Shop/TotalBill.java
+++ Shop/TotalBill.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -18,10 +11,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Amal Sunil
- */
 public class TotalBill extends HttpServlet {
 
     /**
