a99843f44693c3454816e473bb8136d2b90f3015
Simeen Khan Ayyub <55340776+Simeen01@users.noreply.github.com>
2021-09-08 19:57:56 +0530

    Update Stationary.java

--- Shop/Stationary.java
+++ Shop/Stationary.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -13,10 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Amal Sunil
- */
 public class Stationary extends HttpServlet {
 
     /**
