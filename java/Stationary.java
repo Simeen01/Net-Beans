7280f8930c2ef1ea8480fe7477bfe96b9695a494
Simeen Khan Ayyub <55340776+Simeen01@users.noreply.github.com>
2021-09-08 19:47:25 +0530

    Add files via upload

---
+++ java/Stationary.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+//import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Amal Sunil
+ */
+public class Stationary extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+//        out.println("HELLOWORLD");
+        try{
+            /* TODO output your page here. You may use following sample code. */
+                        
+            String str1=request.getParameter("book");
+            String str2=request.getParameter("qty");
+            Cookie c1=new Cookie(str1,str2);   //creating an object of cookie (name value pair)
+            response.addCookie(c1);    //adding the cookie to the response 
+            
+            out.println("<html><body><form method='post' action='TotalBill'>");
+            out.println("<h3>Shop for Stationary</h3> Name of Product <input type='text' name='n1'><br> ");
+            out.println("<br>Quantity <input type='text' name='n2'><br> ");            
+            out.println("<input type='submit' value='SUBMIT'></html></body>");
+
+        }
+        catch(Exception e){
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold
+
+}
