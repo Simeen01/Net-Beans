367c156457bbc8f2136ec9d8040836881bf2cb7f
Simeen Khan Ayyub <55340776+Simeen01@users.noreply.github.com>
2021-09-08 19:59:32 +0530

    Update Book.java

--- firstjdbc/Book.java
+++ firstjdbc/Book.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package firstjdbc;
 
 import java.sql.*;
@@ -18,10 +13,6 @@ import javax.swing.table.*;
 import javax.swing.*;
 import java.awt.*;
 
-/**
- *
- * @author simeenkhan
- */
 public class Book extends javax.swing.JFrame {
 
     /**
